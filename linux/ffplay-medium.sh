b042f9c08f83f784023379b6559ab64bfda7f260
Amran <smamran.com@gmail.com>
2025-09-18 16:56:47 +0600

    added medium res config of manim

---
+++ linux/ffplay-medium.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Activate the virtual environment
+source ~/dev/python/manimcs/venv/bin/activate
+
+# Set the Manim preview command to ffplay with looping
+export MANIM_PREVIEW_COMMAND="ffplay -loop 0"
+
+# Run the Manim command
+manim -pqm demo.py Demo
